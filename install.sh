97ead3a41c274fb071aab4cefa852673762e4261
Zephyr UCH-2009 <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-09-11 09:35:35 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,230 @@
+# ----------- Script UCH 2009 -------------
+# ------ Nomer Admin 085600685685 ---------
+# cannot copy code ngotak tolol kaya gak sekolah ajink
+# tools Versi 0.1 6 Sep 23
+
+clear
+pkg update && pkg upgrade
+pkg install python
+pkg install git
+pip install requests
+pkg install python-pip
+pkg install python2
+
+# Code Warna
+
+R="$(printf '\033[31m')"  
+G="$(printf '\033[32m')"  
+O="$(printf '\033[33m')"  
+BL="$(printf '\033[34m')"
+MAG="$(printf '\033[35m')"  
+CY="$(printf '\033[36m')"  
+W="$(printf '\033[37m')" 
+B="$(printf '\033[30m')"
+
+# Pengulangan 
+
+while [ "$else" = "$ulang" ]
+do
+clear
+echo " "
+echo " "
+echo -e $G"Loading…
+"
+echo -e $O"█▒▒▒▒▒▒▒▒▒
+"
+echo -e $G"10%
+"
+sleep 1
+clear
+echo " "
+echo " "
+echo -e $G"Loading…
+"
+echo -e $O"███▒▒▒▒▒▒▒
+"
+echo -e $G"30%
+"
+sleep 1
+clear
+echo " "
+echo " "
+echo -e $G"Loading…
+"
+echo -e $O"█████▒▒▒▒▒
+"
+echo -e $G"50%
+"
+sleep 1
+clear
+echo " "
+echo " "
+echo -e $G"Loading…
+"
+echo -e $O"███████▒▒▒
+"
+echo -e $G"100%
+"
+sleep 1
+clear
+echo " "
+echo " "
+echo -e $G"Loading…
+"
+echo -e $O"██████████"
+echo " "
+echo -e $W"Selesai masuk sedang memeriksa bahasa yang di pilih "
+sleep 1
+clear
+
+# Login Tools Spam Call
+
+echo " "
+echo -e ${G}"┏┓┏ ┏━┓ ┏┳┓ ┏━┓ ┈ ┏┓┈ ┳┈┳ ┏━┓ ┏┳┓ ┈ ┏━┓ ┏━┓ ┏┓┏ ┏━┓ ┏━┓ ┳┈┳ ┏┓┏ ┏━┓  "
+echo -e ${G}"┃┃┃ ┃┈┃ ┈┃┈ ┣┫┈ ┈ ┣┻┓ ┃┈┃ ┣━┫ ┈┃┈ ┈ ┣━┛ ┣┫┈ ┃┃┃ ┃┈┳ ┃┈┳ ┃┈┃ ┃┃┃ ┣━┫  "
+echo -e ${G}"┛┗┛ ┗━┛ ┈┻┈ ┗━┛ ┈ ┗━┛ ┗━┛ ┻┈┻ ┈┻┈ ┈ ┻┈┈ ┗━┛ ┛┗┛ ┗━┛ ┗━┛ ┗━┛ ┛┗┛ ┻┈┻  "
+echo " "
+echo " "
+echo "${W} PERHATIAN  => ${O} SEBELUM MASUK TOOLS SALIN KODE TOKEN DI BAWAH INI BUAT LOGIN"
+echo "${W} TOKEN => ${G}csp0uK2eHZ6fxW8Pqz3Snp "
+echo "${W} SESUDAH COPY TOKEN NYA TINGGAL LANJUT KETIK (YA) "
+echo " "
+
+# Password
+
+echo
+read -p " SUDAH PAHAM KALO SUDAH KETIK ${O}(${G}YA${O}) ${W}BUAT MASUK TOOLSNYA => " apa 
+if [ "$apa" = "YA" ]
+then
+echo -ne ".........SEDANG MASUK TUNGGU BENTAR SABAR TOD HEE...\r"
+
+# PENGULANGAN 
+
+while [ "$else" = "$ulang" ]
+do
+
+#SCRIPT
+
+clear
+echo " "
+echo "${BL}╔══════════════════════════════════════════╗"
+echo "║${R} SILAKAN PILIH OPERASI SESUAI KEBUTUHAN ${BL}  ║"
+echo "${BLUE}╠════╦═════════════════════════════════════╣"
+echo -e "${BL}║\e[32m[\e[33m01\e[32m]${BL}║${W} MASUK TOOLS                         ${BL}║ "
+echo -e "${BL}║\e[32m[\e[33mUP\e[32m]${BL}║${W} UPDATE                              ${BL}║"
+echo -e "${BL}║\e[32m[\e[33mTK\e[32m]${BL}║${W} YANG LUPA TOKEN                     ${BL}║"
+echo -e "${BL}║\e[32m[\e[33mKL\e[32m]${BL}║${W} KELUAR / LOGOUT                     ${BL}║"
+echo "${BL}╚════╩═════════════════════════════════════╝"
+echo " "
+echo -e $G "┏⟦ "${W}"Pilih OPERASI YANG TUAN INGINKAN "${G}":)"
+read -p" ${G}┗☞ ${O}Enter Your Number ${W}:" no
+echo " "
+clear
+
+# BANNER SCRIPT
+
+echo -e "${W}╔╦╗ ╔═╗ ╔═╗ ╦     ╔═╗ ╦═╗ ╔═╗ ╔╦╗   ╦┈╦ ╔═╗ ╦ ╦  "
+echo -e "${W} ║  ║ ║ ║ ║ ║     ╠╣  ╠╦╝ ║ ║ ║║║   ║ ║ ║   ╠═╣  "
+echo -e "${W} ╩  ╚═╝ ╚═╝ ╩═╝   ╚   ╩╚═ ╚═╝ ╩┈╩   ╚═╝ ╚═╝ ╩┈╩  "
+echo "${W}[${G} TIPE INSTALLASI TERMUX , LINUX , NETHUNTER ${W}]"
+echo " "
+echo
+
+# SCRIPT SPAM V1
+
+if [ "$no" = "01" ]
+then
+echo " "
+echo "${R} ┏┳┓ ┏━┓ ┳━┓ ┏┳┓ ┳ ┳ ━┓ ┳    "
+echo "${R}  ┃  ┣┫  ┣┳┛ ┃┃┃ ┃ ┃ ┏┻┳┛    [ ${W} Software Termux ${R}] "
+echo "${R}  ┻  ┗━┛ ┻┗━ ┻ ┻ ┗━┛ ┻ ┗━    "
+sleep 1s
+echo "${W} Script By : UCH 2009 FROM"
+sleep 1s
+echo  " "
+echo "${BL}[${R}!${BL}] ${G}Tuan Mengunakan Software installasi Termux tunggu sampai instalasinya selesai"
+sleep 1s
+echo "${BL}[${R}✓${BL}] ${G}Loading Installing In Termux..."
+echo " "
+sleep 2s
+echo "~{${O} Bersiap Untuk Menginstal Tuan ${G}}~"
+sleep 3s
+echo " "
+git clone https://github.com/Unlimited-Crack-You/SpamV2.git
+cd SpamV2
+python bot.py
+
+# UPDATE
+
+elif [ "$no" = "UP" ]
+then
+echo " "
+echo "${R}┳ ┳ ┏━┓ ┏┳┓ ┏━┓ ┏┳┓ ┏━┓   ┏┳┓ ┏━┓ ┏━┓ ┳   ┏━┓  "
+echo "${R}┃ ┃ ┣━┛  ┃┃ ┣━┫  ┃  ┣┫     ┃  ┃ ┃ ┃ ┃ ┃   ┗━┓  "
+echo "${R}┗━┛ ┻   ━┻┛ ┻ ┻  ┻  ┗━┛    ┻  ┗━┛ ┗━┛ ┻━┛ ┗━┛  "
+echo "${W} Script By : UCH 2009 "
+sleep 1s
+echo  " "
+echo "${BL}[${R}!${BL}] ${G}Tuan Mengunakan Software Termux saat masuk Script Update  "
+sleep 1s
+echo "${BL}[${R}✓${BL}] ${G}Loading Installing Update in Termux.."
+    echo " "
+      sleep 2s
+   echo "~{${O} Bersiap Untuk Mengupdate Tools ${G}}~"
+cd $HOME
+cd
+rm -rf command
+git clone https://github.com/Unlimited-Crack-You/SpamV2.git
+SpamV2
+chmod +x *
+git pull
+bash install.sh
+
+# KELUAR / LOGOUT 
+
+elif [ "$no" = "KL" ]
+then
+echo " "
+echo "${R} ╦    ╔═╗ ╔═╗ ╔═╗ ╦ ╦ ╔╦╗   ╔═╗ ╔═╗ ╦═╗ ╦ ╔═╗ ╔╦╗ "
+echo "${R} ║    ║ ║ ║ ╦ ║ ║ ║ ║  ║    ╚═╗ ║   ╠╦╝ ║ ╠═╝  ║"
+echo "${R} ╩═╝  ╚═╝ ╚═╝ ╚═╝ ╚═╝  ╩    ╚═╝ ╚═╝ ╩╚═ ╩ ╩    ╩ ${O}[ ${W}Logout installasi ${O}]"
+sleep 1s
+echo "${W} Script By : UCH 2009 "
+sleep 1s
+echo  " "
+echo "${BL}[${R}!${BL}] ${G}Tuan Sedang Proses Keluar Tunggu TOD"
+sleep 1s
+echo "${BL}[${R}✓${BL}] ${G}Loading Logout In Software From..."
+echo " "
+sleep 2s
+echo "~{${O} TUUAN TELAH LOGOUT DARI FROM UCH BAY BAY ${G}}~"
+sleep 3s
+echo " "
+echo -e $W"KHO :)" 
+echo -e $G"THANK'S Sampai Jumpa di Kemudian Hari"
+echo -e $W"BY ${G}: ${O}UCH 2009 FROM"
+sleep 3
+clear
+exit
+else
+clear
+sleep 1
+echo " Tools Spam UC-09 Selamat menggunakan dengan baik  "
+sleep 1
+clear
+fi
+done
+else
+
+# SALAH PASSWORD 
+
+clear
+sleep 2
+echo -ne $q $m" TOD MASUKIN YANG BENER UDAH JELAS DI SURUH KETIK YA MALAH KETIK YANG LAIN 🗿\r"
+sleep 3
+echo -ne $q $h" YAH UDAH KETIK YANG BENER SUBSCRIBE DULU CHANNEL GWE\r"
+xdg-open "https://www.youtube.com/@from-system-comunity"
+sleep 3
+clear
+fi
+done
